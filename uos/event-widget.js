0f1c5a9b3c0d3a7a42a8e04568d5e5dd3f9600ab
Twist <59713304+andrewtwist859@users.noreply.github.com>
2024-11-16 11:58:48 +0000

    Update event-widget.js

--- uos/event-widget.js
+++ uos/event-widget.js
@@ -338,9 +338,35 @@
         eventList.slice(0, displayedEventCount + eventsPerPage).forEach(event => {
           container.innerHTML += \`
             <div class="event">
-              <h3>\${event.Name}</h3>
-              <p>Date: \${event.Date}</p>
+            <a href="${URL}" target="_blank">
+              <img src="${Thumbnail || 'https://via.placeholder.com/150'}" alt="${Name} Thumbnail">
+            </a>
+            <div class="event-content">
+              <a href="${URL}" target="_blank">
+                <h3>${Name}</h3>
+              </a>
+              <div class="event-details">
+                <p>Date: <span>${Date}</span></p>
+                <p>Time: <span>${StartTime || ''} - ${EndTime || ''}</span></p>
+                <p>Location: <span>${Location}</span></p>
+              </div>
+              <p>${Description}</p>
+              <div class="link">
+                <a href="${URL}">
+                  ${URL ? 'Book your place' : ''}
+                  <span class="link-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                      <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
+                    </svg>
+                  </span>
+                </a>
+              </div>
+              <div class="flags">
+                <span class="flag">${EventType}</span>
+                ${Tags ? Tags.split(',').map(tag => `<span class="flag">${tag.trim()}</span>`).join('') : ''}
+              </div>
             </div>
+          </div>
           \`;
         });
         displayedEventCount += eventsPerPage;
